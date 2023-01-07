44fc6ef70b0ebf358e835491301eb39c25a0c019
D111sP <115263492+D111sP@users.noreply.github.com>
2023-01-07 20:13:43 +0500

    тест

--- front/get_audio.js
+++ front/get_audio.js
@@ -1,10 +1,9 @@
-doc = document
 console.log("123")
 let xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
-        chech_fun(this.responseText)
+        chech_fun(JSON.parse(this.responseText))
     };
 };
 
@@ -12,6 +11,6 @@ xhttp.open("GET", "http://127.0.0.1:8000/audio", true);
 xhttp.send();
 
 function  chech_fun(data){
-    doc.getElementById("audio").src = "data:audio/mpeg;base64," + data
-    doc.load();
+    console.log(data)
+    document.getElementById("audio").src = data.key;
 };
